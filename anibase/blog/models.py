0e01487f432f346c2aafb750a96229e1a2e88937
Doppelgangers <6666genocide666@mail.ru>
2023-09-05 23:20:22 +0300

    Changed the structure of the database

--- anibase/blog/models.py
+++ anibase/blog/models.py
@@ -1,51 +1,69 @@
 from django.contrib.auth.models import User
 from django.db import models
-from django.shortcuts import reverse
+
 # Create your models here.
 
 
-class Artwork (models.Model):
-    title = models.CharField(max_length=255, verbose_name='Название')
+class Artwork(models.Model):
+    SEASON_CHOICES = [('w', 'Зима/Winter'),
+                      ('s', 'Вестна/Spring'),
+                      ('a', 'Осень/Autumn'),
+                      ('s', 'Лето/Summer'),
+                      ('n', 'None')
+                      ]
+
+    """Название"""
+    title = models.CharField(max_length=256, verbose_name='Название')
     english_title = models.CharField(max_length=255, verbose_name='Англиское название', blank=True, null=True)
-    image = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name='Обложка', null=True)
+
+    """Пренадлежит серии книг/сериалов/..."""
+    series = models.ForeignKey('SeriesArtwork', on_delete=models.PROTECT)
+    series_number = models.IntegerField(verbose_name="Номер тома или сезона", null=True, blank=True, default=1)
+
+    """Обложка"""
+    image = models.ImageField(upload_to="artwork_covers/%Y/", verbose_name='Обложка', null=True, blank=True)
+
+    """Количество глав в книге/ серий в сериале"""
+    episodes = models.IntegerField(verbose_name="Количество глав/серий")
+
+    """К какой категории принадлежит работа, книга/сериал/новела/..."""
+    category = models.ForeignKey('CategoryArtwork', models.PROTECT, verbose_name='Категория')
+
+    """Год и сезоне когда вышло произведение зима/вестна/лето/осень"""
     year = models.IntegerField(verbose_name="Год", blank=True, null=True)
+    year_season = models.CharField(max_length=1, choices=SEASON_CHOICES, default='n')
+
+    """Оценка и описание"""
     rating = models.IntegerField(verbose_name="Оценка", blank=True, null=True)
     description = models.TextField(blank=True, verbose_name="Описание")
 
-    number = models.IntegerField(verbose_name="Номер тома или сезона", null=True, blank=True)
+    """Автор"""
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, verbose_name='Автор', blank=True, null=True)
 
-    author = models.ForeignKey('Author', models.SET_NULL, verbose_name='Автор', blank=True, null=True)
-    category = models.ForeignKey('Category', models.PROTECT, verbose_name='Категория')
-    genre = models.ManyToManyField('Genre', verbose_name="Жанры", blank=True)
-    series = models.ForeignKey('Series', models.PROTECT, verbose_name="Серия", blank=True, null=True)
+    """Пользовательские данные"""
     creator = models.ForeignKey(User, models.SET_NULL, verbose_name='Автор записи', blank=True, null=True)
-
     viewing_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата просмотра', blank=True)
-    create_data = models.DateTimeField(auto_now_add=True, verbose_name="Дата публикации записи", blank=True)
-    slug = models.SlugField(verbose_name="URL", unique=True, db_index=True, blank=True)
+    create_data = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания записи", blank=True)
 
+    slug = models.SlugField(verbose_name="URL", unique=True, db_index=True, blank=True)
     is_active = models.BooleanField(default=True)
 
     class Meta:
         ordering = ['-viewing_date']
 
 
-class Category (models.Model):
-    title = models.CharField(max_length=255 , verbose_name='Название')
-    slug = models.SlugField(verbose_name="URL", unique=True, db_index=True)
+class AlternativeName (models.Model):
+    artwork = models.ForeignKey('Artwork', on_delete=models.CASCADE)
+    title = models.CharField(max_length=256, verbose_name='Альтернативное название')
 
 
-class Genre (models.Model):
-    title = models.CharField(max_length=255, verbose_name='Название')
-    slug = models.SlugField(verbose_name="URL", unique=True, db_index=True)
-    description = models.CharField(max_length=2_000, null=True, verbose_name="Описание")
+class CategoryArtwork (models.Model):
+    name = models.CharField(max_length=256, verbose_name="Название")
 
 
-class Series (models.Model):
-    title = models.CharField(max_length=255, verbose_name='Название')
-    slug = models.SlugField(verbose_name="URL", unique=True, db_index=True)
+class SeriesArtwork(models.Model):
+    title = models.CharField(max_length=256, verbose_name='Название')
 
 
 class Author(models.Model):
-    name = models.CharField(max_length=255, verbose_name='Имя/Псевданим')
-    slug = models.SlugField(verbose_name="URL", unique=True, db_index=True)
+    name = models.CharField(max_length=256, verbose_name="Имя/Псевданим")
